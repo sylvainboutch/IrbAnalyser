4c069f489bce7fc55c5f7ca430693141db40eeec
sylvainboutch <sylvainboutch@gmail.com>
2015-04-06 12:14:44 -0400

    First Entity Oracle test--failed

--- IrbAnalyser/IrbAnalyser/Form1.Designer.cs
+++ IrbAnalyser/IrbAnalyser/Form1.Designer.cs
@@ -28,12 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Location = new System.Drawing.Point(0, 239);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(284, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtOutput.Location = new System.Drawing.Point(0, 0);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtOutput.Size = new System.Drawing.Size(284, 239);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
 
--- IrbAnalyser/IrbAnalyser/Form1.cs
+++ IrbAnalyser/IrbAnalyser/Form1.cs
@@ -15,5 +15,22 @@ namespace IrbAnalyser
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var db = new Models.VelosStg())
+            {
+                var query = from b in db.VDA_V_STUDY_SUMMARY
+                            orderby b.STUDY_NUMBER
+                            select b;
+                var output = "List all study /r/n";
+                foreach (var item in query)
+                {
+                    output += item.STUDY_NUMBER + "/r/n";
+                }
+
+                txtOutput.Text = output;
+            }
+        }
     }
 }
